6d75d6d66ab4f324243667a5571eb9c9c6e37ecc
saiabhilekhareddy24-lab <saiabhilekhareddy24@gmail.com>
2026-08-15 20:43:27 +0530

    Create half_subtractor.v

---
+++ half_subtractor.v
@@ -0,0 +1,15 @@
+// Half Subtractor
+// Inputs : A, B
+// Outputs: Difference, Borrow
+
+module half_subtractor (
+    input  A,
+    input  B,
+    output Difference,
+    output Borrow
+);
+
+    assign Difference = A ^ B;
+    assign Borrow = (~A) & B;
+
+endmodule
